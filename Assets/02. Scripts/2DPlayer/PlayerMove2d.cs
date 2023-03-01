a72bfdaab3985ca98282275ba5d16c4f72d1a8e4
aeju <aejuchoe@naver.com>
2023-03-01 09:21:27 +0900

    Add: 3DPlayerJump

--- Assets/02. Scripts/2DPlayer/PlayerMove2d.cs
+++ Assets/02. Scripts/2DPlayer/PlayerMove2d.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 // 사용자 입력(상/하, 좌/우)에 따라 상하좌우로 이동
 // 필요속성 : 이동속도
+
+// 점프 버튼 누르면 점프
+// 필요속성 : 점프파워, Rigidbody2D
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMove2d : MonoBehaviour
 {
-    // 필요속성 : 이동속도
+    // 필요 속성 : 이동속도
     public float speed = 5;
+    
+    // 필요 속성 : 점프파워
+    public float jumpPower = 2;
+    //Rigidbody2D rb;
+    Rigidbody rb; 
     
     // Start is called before the first frame update
     void Start()
@@ -30,5 +39,16 @@ public class PlayerMove2d : MonoBehaviour
         // 3. 이동하고 싶다.
         // P = P0 + vt
         transform.position += dir * speed * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        // 사용자가 점프버튼을 누르면 점프하고 싶다.
+        // 1. 점프버튼을 눌렀으니까
+        if (Input.GetButtonDown("Jump"))
+        {
+            // 2. 점프하고 싶다.
+            rb.velocity = Vector2.up * jumpPower;
+        }
     }
 }
--- Assets/02. Scripts/3DPlayer/PlayerMove3D.cs
+++ Assets/02. Scripts/3DPlayer/PlayerMove3D.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+    
+// <이동>
 // 사용자의 입력에따라 앞뒤좌우 이동하고 싶다.
 // 필요속성 : 이동속도
+
+// <점프>
+// 중력을 적용시키고 싶다.
+// 필요속성 : 중력, 수직속도
+// 사용자가 점프버튼을 누르면 점프하고 싶다.
+// 필요속성 : 점프파워
 public class PlayerMove3D : MonoBehaviour
 {
     // 필요속성 : 이동속도
     public float speed = 5;
 
     CharacterController cc;
+    
+    // 필요속성 : 중력, 수직속도
+    public float gravity = -20;
+    float yVelocity = 0;
+    // 필요속성 : 점프파워
+    public float jumpPower = 5;
+
+    // 점프중인지 여부 기억
+    bool isJumping = false;
+    int jumpCount = 0;
+    public int jumpCountMax = 2;
     
     // Start is called before the first frame update
     void Start()
@@ -25,6 +43,22 @@ public class PlayerMove3D : MonoBehaviour
         float v = Input.GetAxis("Vertical");
         
         Vector3 dir = new Vector3(h, 0, v);
+        dir = Camera.main.transform.TransformDirection(dir);
+        // v = v0 + at
+        yVelocity += gravity * Time.deltaTime;
+        
+        // 사용자가 점프버튼을 누르면 점프하고 싶다.
+        if (jumpCount < jumpCountMax && Input.GetKeyDown(KeyCode.A))
+        //if (Input.GetButtonDown("Jump"))
+        {
+            print("jump");
+            // -> 수직속도에 변화를 주고싶다.
+            yVelocity = jumpPower;
+            jumpCount++;
+            //isJumping = true;
+        }
+
+        dir.y = yVelocity;
         
         // P = P0 + vt
         cc.Move(dir * speed * Time.deltaTime);
